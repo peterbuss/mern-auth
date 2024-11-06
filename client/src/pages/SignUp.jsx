d747ba4f2d39485d55f2d6d7ec33987b9aa95719
Peter BUSS <peter.buss@hispeed.ch>
2024-11-06 21:03:03 +0100

    complete the sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,14 @@
  /* eslint-disable */
  
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
 
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value});
@@ -34,6 +35,7 @@ export default function SignUp() {
         setError(true);
         return;
       }
+      navigate('/sign-in');
     } catch(error) {
       setLoading(false);
       setError(true);
